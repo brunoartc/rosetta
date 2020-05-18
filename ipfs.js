4a01cbc8d83c563adaaf11d3e8e48c1308c1be18
brunoartc <brunoartc@yahoo.com>
2020-05-18 03:27:14 -0300

    upload File & get Hash and link

--- ipfs.js
+++ ipfs.js
@@ -1,17 +1,11 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
-const IPFS = require('ipfs-api');
-var fs = require("fs");
+const IPFS = require('ipfs-api')
 
-
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
-// Asynchronous read
-fs.readFile('index.html', function (err, data) {
-    if (err) {
-       return console.error(err);
-    }
-    ipfs.add(data , (err, ipfsHash) => {
-        console.log(ipfsHash);
-        
-        }); //storehash 
- });
-
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+obj = {
+    name: "teste"
+}
+var buf = Buffer.from(JSON.stringify(obj))
+ipfs.add(buf, (err, ipfsHash) => {
+  console.log(ipfsHash, "https://cloudflare-ipfs.com/ipfs/" + ipfsHash[0].hash)
+})
